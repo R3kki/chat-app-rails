b7fe200f525814259a828648b275ce2881b261eb
rich <richie.parkie@gmail.com>
2020-10-02 22:17:15 -0400

    added: unique usernames, messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   validates_uniqueness_of :username
+
+  def self.generate
+      adjectives = ['Ancient', 'Bulky', 'Creative', 'Dashing', 'Efficient', 'Flying', 'Golden']
+      nouns = ['Hypeman', 'Iguana', 'Jersey', 'Leopard', 'Monster']
+      
+      number = rand.to_s[2..4]
+      username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+
+      create(username: username)
+  end
 end
