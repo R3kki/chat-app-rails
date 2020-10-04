3a7a3b1ce056fc14d9b1a47454bc87fbde97382b
rich <richie.parkie@gmail.com>
2020-10-03 20:59:31 -0400

    added: separate subscriptions per chat room

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,32 +1,43 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 1}, {
-// consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("connected");
-  },
+// executes after pages loads
+  document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = room_element.getAttribute('data-room-id');
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-    console.log("disconnected");
-  },
+    // user does not reload page and clicks new room needs to reset existing subscriptions
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription)
+  })
+  
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id}, {
+  // consumer.subscriptions.create("RoomChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log("connected");
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    
-    const element = document.getElementById('user-id');
-    const user_id = Number(element.getAttribute('data-user_id'));
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+      console.log("disconnected");
+    },
 
-    let html;
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
 
-    if (user_id === data.message.user_id) {
-      html = data.mine
-    } else {
-      html = data.theirs
+      let html;
+
+      if (user_id === data.message.user_id) {
+        html = data.mine
+      } else {
+        html = data.theirs
+      }
+      const messageContainer = document.getElementById('messages');
+      messageContainer.innerHTML = messageContainer.innerHTML + html
+
     }
-    const messageContainer = document.getElementById('messages');
-    messageContainer.innerHTML = messageContainer.innerHTML + html
-
-  }
-});
+  });
+})
